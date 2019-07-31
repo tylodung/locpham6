534b71e9b02acbda4c2abfcc81e2c72d2dc6b917
TY LO DUNG <doan.trong.quoc.binh@gmail.com>
2019-07-31 08:46:53 +0700

    up

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () =>
     <footer className="site-footer outer">
         <div className="site-footer-content inner">
             <section className="copyright">
-                <Link to="/">Studiotechweb.com</Link> | Phượt Ký
+                <Link to="/">Ethoreka.com</Link> | Lên Đường
             </section>
             <nav>
                 <a className="social-logo" href="https://youtu.be/N4WTi9EHLqA" >
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -31,7 +31,7 @@ const Template = ({ data }) => {
 
       <Navbar />
 
-      <Helmet title={`Phượt Ký | ${post.frontmatter.title}`} />
+      <Helmet title={`Lên Đường | ${post.frontmatter.title}`} />
 
       <div className="blog-post-header" style={{ backgroundImage: `url(${ post.frontmatter.image })` }}>
 
