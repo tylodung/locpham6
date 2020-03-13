ced5a9128a10ce1dfcbe1cddedae52a3fd29ecbd
TY LO DUNG <doan.trong.quoc.binh@gmail.com>
2020-03-13 08:41:39 +0700

    up

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () =>
     <footer className="site-footer outer">
         <div className="site-footer-content inner">
             <section className="copyright">
-                <Link to="/">laodao.info</Link> Copyright © Nhật Ký Của Tôi
+                <Link to="/">Nhật Ký Của Tôi</Link> Copyright © 2020
             </section>
             <nav>
                 <a className="social-logo" href="https://youtu.be/N4WTi9EHLqA" >
